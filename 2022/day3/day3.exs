3bf77d37238df63e85fb9205f9267a75a0f6bbfd
haglobah <bah@posteo.de>
2022-12-05 14:00:37 +0100

    Add day3

--- 2022/day3/day3.exs
+++ 2022/day3/day3.exs
@@ -1,7 +1,11 @@
 defmodule Day3 do
 
+  def char_to_prio([c]) when c >= 97 and c <= 122 do c - 96 end
+  def char_to_prio([c]) when c >= 65 and c <= 90 do c - 38 end
+
+  def get_same_prio() do end
   def main() do
-    input = File.read!("input3.txt")
+    input = File.read!("input.txt")
     test = "AjlkaQwErk\r\nuipohyxcvh\r\n"
     test2 = "vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
@@ -10,24 +14,16 @@ wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw"
 
-    num = fn
-       [c] when c >= 97 and c <= 122 -> c - 96
-       [c] when (c >= 65) and (c <= 90) -> c - 38
-    end
-
-    get_same = fn s1, s2 ->
-      MapSet.intersection(MapSet.new(s1), MapSet.new(s2))
-      |> MapSet.to_list |> Enum.map(&(num.(to_char_list(&1))))
+    get_same = fn r1, r2, r3 ->
+      MapSet.intersection(MapSet.new(r1), MapSet.intersection(MapSet.new(r2), MapSet.new(r3)))
+      |> MapSet.to_list |> Enum.map(&(char_to_prio(to_charlist(&1))))
       #|> &(num.(&1))
     end
 
-    get_prio_value = fn s ->
-      len = String.length(s)
-      comps = s |> String.split_at(trunc(len/2))
-      |> Tuple.to_list()
-      |> Enum.map(&(String.graphemes(&1) |> MapSet.new()))
+    get_prio_value = fn rucksacks ->
+      comps = rucksacks |> Enum.map(&(String.graphemes(&1) |> MapSet.new()))
 
-      get_same.(Enum.at(comps, 0), Enum.at(comps, 1))
+      get_same.(Enum.at(comps, 0), Enum.at(comps, 1), Enum.at(comps, 2))
       #|> Enum.map(&(get_elem_value()))
     end
 
